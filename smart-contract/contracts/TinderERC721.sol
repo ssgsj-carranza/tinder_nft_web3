d412c3a6e5acec787e644ebacb3e39bf69ff08dd
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-28 12:04:59 -0500

    began work on contract for erc721 per documentation

--- smart-contract/contracts/TinderERC721.sol
+++ smart-contract/contracts/TinderERC721.sol
@@ -1,1 +1,16 @@
 // SSPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils.Counters./sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract TinderERC721 is ERC721URIStorage {
+    uint256 TINDER_TOKEN_ID;
+    constructor() ERC721("CleverNFT", "CN") {}
+
+    function mintNFT(address _userOne, address _userTwo, string memory tokenURI) public {
+        
+    }
+};
+
